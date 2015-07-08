4da8b106180b6511aa2cb6759457b4784c75ba77
wolfgangfoulkes <wolfgangfoulkes@alum.calarts.edu>
2015-07-08 07:17:09 -0700

    moved method to MODEL obviously. REMEMBER THAT CUSTOM METHODS ARE IN THE MODEL, AND ACTIONS BASED ON ROUTES ARE IN THE CONTROLLER

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,18 @@ class User < ActiveRecord::Base
 	#----- socialization -----
 	acts_as_follower
 	#-----
+
+	def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+        data = access_token.info
+        user = User.find_by(email: data.email)
+        if user
+            user.provider = access_token.provider
+            user.uid = access_token.uid
+            user.token = access_token.credentials.token
+            user.save 
+            user
+        else 
+            redirect_to new_user_registration_path, notice: "Google Error!"
+        end
+    end
 end
