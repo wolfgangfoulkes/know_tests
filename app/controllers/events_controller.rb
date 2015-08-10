a73ff56bd49f6fb3c1eeff92ac48a6955b70adf5
wolfgangfoulkes <wolfgangfoulkes@alum.calarts.edu>
2015-08-09 22:48:43 -0700

    event edits and updates restricted

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,4 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   def index
     @events = Event.filter(params.slice(:name_starts_with, :name_contains))
@@ -13,14 +12,14 @@ class EventsController < ApplicationController
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def edit
-    @user = User.find(@event.user_id)
+    @event = current_user.events.find(params[:id])
   end
 
   def new
-    @user = current_user
     @event = Event.new
   end
 
@@ -38,6 +37,7 @@ class EventsController < ApplicationController
   end
 
   def update
+    @event = current_user.events.find(params[:id])
     @event.update(event_params)
     respond_to do |format|
       if @event.save
@@ -51,6 +51,7 @@ class EventsController < ApplicationController
   end
 
   def destroy
+    @event = current_user.events.find(params[:id])
     @event.destroy
     respond_to do |format|
       format.html { redirect_to root_url, notice: 'Event was successfully destroyed.' }
@@ -62,40 +63,7 @@ class EventsController < ApplicationController
     render partial: "follow_button"
   end
 
-  def add_to_calendar
-    @event = Event.find(params[:id])
-    @data = 
-    {
-        'summary' => @event.name,
-        'description' => @event.description,
-        'location' => 'Somewhere in Nevada',
-        'start' => {
-          'dateTime' => @event.starts_at.to_s(:iso8601),
-          'timeZone' => 'America/Los_Angeles',
-        },
-        'end' => {
-          'dateTime' => @event.ends_at.to_s(:iso8601),
-          'timeZone' => 'America/Los_Angeles',
-        }
-    }
-
-    client = Google::APIClient.new
-    token = Token.find_by email: current_user.email
-    client.authorization.access_token = token.fresh_token
-    service = client.discovered_api('calendar', 'v3')
-    result = client.execute!(
-      :api_method => service.events.insert,
-      :parameters => {'calendarId' => 'primary', 'sendNotifications' => true},
-      :body_object => @data,
-      :headers => {'Content-Type' => 'application/json'}
-      )
-    @data = result.data
-  end
-
   private
-    def set_event
-      @event = Event.find(params[:id])
-    end
 
     def event_params
       params.require(:event).permit(:name, :starts_at, :ends_at, :description, :user_id, :tag_list)
