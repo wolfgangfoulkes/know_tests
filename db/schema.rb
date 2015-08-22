a9843c73ef31f38e3498a6b19010f1129c56659b
wolfgangfoulkes <wolfgangfoulkes@alum.calarts.edu>
2015-08-21 21:59:13 -0700

    cleaned up schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,17 +16,8 @@ ActiveRecord::Schema.define(version: 20150811155829) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "author_comments", force: true do |t|
-    t.integer  "event_id"
-    t.string   "subject"
-    t.string   "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "author_comments", ["event_id"], name: "index_author_comments_on_event_id", using: :btree
-
   create_table "comments", force: true do |t|
+    t.string   "type"
     t.integer  "commentable_id"
     t.string   "commentable_type"
     t.string   "subject"
