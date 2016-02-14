adcd9d8ed9e530865542359ca803ef394a546883
wolfgangfoulkes <wolfgangfoulkes@alum.calarts.edu>
2016-02-13 16:47:28 -0800

    ...

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,9 +13,4 @@ class StaticPagesController < ApplicationController
     activities = PublicActivity::Activity.where(owner: (current_user.followees(Event) | current_user.events) )
     @events = Event.where(id: activities.order("created_at DESC").pluck("owner_id") )
   end
-
-  def search_test
-    @user = current_user
-    @events = Event.all
-  end
 end
