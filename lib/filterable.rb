40ea4824b4c495d2e3a0e3b1a5d12db3f144745d
wolfgangfoulkes <wolfgangfoulkes@alum.calarts.edu>
2016-03-13 18:40:25 -0700

    ...

--- lib/filterable.rb
+++ lib/filterable.rb
@@ -6,6 +6,32 @@ module Filterable
     end
   end
 
+  #----- some arel utility functions originally in Event -----#
+  #- I haven't extensively tested them
+  #---
+  def self.s_to_arel(q)
+    Arel::Nodes::SqlLiteral.new(q)
+  end
+  # for ActiveRecord Relations based on string conditions
+  # e.g. Event.where("name ILIKE '%b'")
+  def self.q_to_arel(q)
+    q.where_values.map{ |v| v.class.name == "String" ? self.s_to_arel(v) : v }.reduce(:or)
+  end
+  
+  def self.to_arel(q)
+    if q.class.name == "String"
+      self.s_to_arel(q)
+    elsif q.class.name == "ActiveRecord::Relation"
+      self.q_to_arel(q)
+    else
+      nil
+    end
+  end
+
+  def self.rel2arel(rel)
+    rel.arel.constraints[0] || false
+  end
+
   module ClassMethods
     def filter(filtering_params)
       results = self.where(nil)
@@ -31,8 +57,12 @@ module Filterable
       #hash params always arel
     end
 
-    def rel2arel(rel)
-      rel.arel.constraints[0] || false
+    # second part works only with ActiveRecord hash queries or Arel queries:
+    # e.g. {name: }
+    def hor(q)
+      self.where_values.reduce(:or)
     end
+    #-----#
+
   end
 end
