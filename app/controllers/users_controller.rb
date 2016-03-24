7c30d7f2bd25e9ed1c69f2795e1f402fa088f0d2
wolfgangfoulkes <wolfgangfoulkes@alum.calarts.edu>
2016-03-24 10:51:52 -0700

    users controller is now useless

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,9 @@ class UsersController < ApplicationController
 	load_and_authorize_resource
 
 	def show
-		@events = @user.events.page( params[:page] ).per(8)
+		@events = @user.events
 		respond_to do |format|
       	  format.html 
-	      format.js {
-	        render "shared/paginate.js.erb", locals: {target: "events", content: @events.page(params[:page]).per(2) }
-	      }
 	    end
 	end
 
